db5b10161125de6c441d3256c4b8913b700f040a
AhmedShafee-dev <nrwege68@gmail.com>
2025-10-25 22:42:51 +0300

    Update

--- src/pages/shopSection/ShopData.jsx
+++ src/pages/shopSection/ShopData.jsx
@@ -53,8 +53,9 @@ export default function ShopData({ item }) {
                             <div className="flex gap-15">
                                 <div className="flex flex-col">
                                     <h2 className="text-xl font-bold mt-4">{item.title}</h2>
-                                    <p className="text-gray-600 mt-2">Subject : {item.description}</p>
-                                    {/* <span>Rate [ from 1 to 5 ] : {item.rate}</span> */}
+                                    <p className="text-gray-600 mt-2">About : {item.description}</p>
+                                    <span className="text-gray-600 mt-2">Price is : {item.Price}</span>
+                                    <span className="text-gray-600 mt-2">subject : {item.subject}</span>
                                 </div>
                             </div>
                         </div>
